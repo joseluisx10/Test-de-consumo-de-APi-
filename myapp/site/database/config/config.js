53338005d39b8a2d0c65d6dc98800eb37680ae0b
joseluisx10 <joseluis.9108@hotmail.com>
2021-04-30 18:06:03 -0300

    intalacion de sequelize y .dotenv=variables locales

--- myapp/site/database/config/config.js
+++ myapp/site/database/config/config.js
@@ -1,9 +1,9 @@
 module.exports = {
   "development": {
-    "username": "root",
-    "password": null,
-    "database": "testtobs",
-    "host": "localhost",
+    "username":  process.env.DB_USER,
+    "password": process.env.DB_PASS,
+    "database": process.env.DB_DATABASE,
+    "host": process.env.DB_HOST,
     "dialect": "mysql"
   },
   "test": {
