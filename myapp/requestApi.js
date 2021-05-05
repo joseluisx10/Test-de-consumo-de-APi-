835eeabc56e82b5331c6707eaeb98526f0716905
joseluisx10 <joseluis.9108@hotmail.com>
2021-05-04 23:17:59 -0300

    usando variables

--- myapp/requestApi.js
+++ myapp/requestApi.js
@@ -1,15 +1,15 @@
+require ('dotenv').config()
 const fetch = require("node-fetch");
 const fs = require('fs');
 const dirJuegos = __dirname +'/public/file/juegos.json' ;
 
-fetch('https://www.freetogame.com/api/games')
+fetch(process.env.API_URL)
 .then((respuesta)=>{
     return respuesta.json();
 })
 .then(resultado =>{
     let leerArchivoJson= fs.readFileSync(dirJuegos, 'utf8');
     if(leerArchivoJson.length == 0){
-        console.log(resultado)
         fs.writeFileSync(dirJuegos, JSON.stringify(resultado));
             
     }
